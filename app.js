cf1b9488f3b1d395822a353739fb188390201989
hencore219 <58836623+hencore219@users.noreply.github.com>
2023-08-10 00:41:13 -0700

    first commit through bash

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 function exit(){
     alert('Exiting Session');
+    if(confirm("are you sure you want to exit?"))
+    {
+        window.location="https://google.com";
+    }
+    else{
+        window.location="index.html";
+    }
 }
 
 function checkin(){
@@ -8,4 +15,5 @@ function checkin(){
 
 function bagtag(){
     alert('printing tags');
+    if(confirm(prompt("") + "how many tags?"));
 }
